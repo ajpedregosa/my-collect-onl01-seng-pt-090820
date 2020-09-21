2a75f83c9aec890734267b7e58435a810be288a5
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 20:13:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+require 'pry'
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
 
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+def 
+
+end
