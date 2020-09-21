172a7b1f430b5cbf40252f21340958fd4acd6ff2
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 20:20:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,6 +4,8 @@ def my_collect(array)
   array.split(" ").first{|name|}
 end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) {|lang| lang.upcase
 
 require 'pry'
 
