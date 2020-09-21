b5049c53d6974eefdb7833bf1e0350b38d110e95
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 20:22:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++
@@ -1,8 +0,0 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-def my_collect(array)
-  array.split(" ").first{|name|}
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) {|lang| lang.upcase}
