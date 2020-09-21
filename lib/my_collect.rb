c8bf96c903cb6820065cdb2750dfe94ee888cadf
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 20:16:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,14 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-def my_collect(array)
+def my_collect(array) 
   i = 0
   collecttion 
 end
 
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
 require 'pry'
 
